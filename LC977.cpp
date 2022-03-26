b1178e6cd704316f78aed6928232b6924fbb6012
10-disha <jayswaldisha5@gmail.com>
2022-03-26 14:50:22 +0530

    Day 2 done

---
+++ LC977.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        vector<int> res(nums.size());
+        int l = 0;
+        int r = nums.size() - 1;
+
+        for (int k = nums.size() - 1; k >= 0; k--)
+        {
+            if (abs(nums[r]) > abs(nums[l]))
+            {
+                res[k] = nums[r] * nums[r--];
+            }
+
+            else
+            {
+                res[k] = nums[l] * nums[l++];
+            }
+        }
+
+        return res;
+    }
+};
